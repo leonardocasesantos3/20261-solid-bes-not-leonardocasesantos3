10bd65566e03cbf3d5a080c83f6654a150a39524
leonardocasesantos3 <leonardocase.santos@ucsal.edu.br>
2026-04-09 17:58:08 -0300

    Implementação da classe Tabuleiro (SRP)
    fazer a criação do tabuleiro visual por uma classe própria em vez da classe App

--- src/main/java/br/com/ucsal/olimpiadas/App.java
+++ src/main/java/br/com/ucsal/olimpiadas/App.java
@@ -163,7 +163,7 @@ public class App {
 			System.out.println(q.getEnunciado());
 
 			System.out.println("Posição inicial:");
-			imprimirTabuleiroFen(q.getFenInicial());
+			Tabuleiro.imprimirFen(q.getFenInicial());
 
 			for (var alt : q.getAlternativas()) {
 			    System.out.println(alt);
@@ -243,41 +243,6 @@ public class App {
 			return null;
 		}
 	}
-
-	static void imprimirTabuleiroFen(String fen) {
-
-		String parteTabuleiro = fen.split(" ")[0];
-		String[] ranks = parteTabuleiro.split("/");
-
-		System.out.println();
-		System.out.println("    a b c d e f g h");
-		System.out.println("   -----------------");
-
-		for (int r = 0; r < 8; r++) {
-
-			String rank = ranks[r];
-			System.out.print((8 - r) + " | ");
-
-			for (char c : rank.toCharArray()) {
-
-				if (Character.isDigit(c)) {
-					int vazios = c - '0';
-					for (int i = 0; i < vazios; i++) {
-						System.out.print(". ");
-					}
-				} else {
-					System.out.print(c + " ");
-				}
-			}
-
-			System.out.println("| " + (8 - r));
-		}
-
-		System.out.println("   -----------------");
-		System.out.println("    a b c d e f g h");
-		System.out.println();
-	}
-
 
 	static void seed() {
 
